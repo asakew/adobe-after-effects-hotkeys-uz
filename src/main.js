0d3fa6e9cb7ab88793a3efd577f39e103df34a34
Akmal Sindorov <asakew@ya.ru>
2021-01-30 16:10:20 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const currentVersion = '1.0.2';
-fetch('https://raw.githubusercontent.com/asakew/full-kew/master/yandex-manifest.json')
+fetch('https://raw.githubusercontent.com/asakew/full-kew/master/src/yandex-manifest.json')
   .then(res => res.json())
   .then(manifest => {
     if (currentVersion !== manifest.yandex.app_version) {
